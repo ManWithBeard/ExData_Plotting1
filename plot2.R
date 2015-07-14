0c18e271df33fbc06f9a372e54cd521fdf1f604c
ManWithBeard <michael.voytinsky@gmail.com>
2015-07-13 21:56:26 -0400

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,16 @@
+plot2 <- function()
+{ 
+  
+  power_consumption <- read.csv2("household_power_consumption.txt")  
+  
+  time_of_interest <- subset(power_consumption, power_consumption[1] == "1/2/2007" | power_consumption[1] == "2/2/2007")
+  
+  global_active <- as.numeric(as.vector(time_of_interest$Global_active_power))
+  
+  
+  plot(global_active, type = 'l', xaxt = "n", ylab = "Global Active Power (kilowatts)", xlab = "")
+  
+  axis(1, at = c(1,1440,2880), labels = c("Thu","Fri","Sat"))
+  
+  global_active
+}
