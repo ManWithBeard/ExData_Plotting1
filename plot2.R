ff901431865c6b11acd837fd90381fa0603b7d10
ManWithBeard <michael.voytinsky@gmail.com>
2015-08-05 01:19:38 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -7,10 +7,13 @@ plot2 <- function()
   
   global_active <- as.numeric(as.vector(time_of_interest$Global_active_power))
   
+  png("plot2.png", height = 480, width = 480)
   
   plot(global_active, type = 'l', xaxt = "n", ylab = "Global Active Power (kilowatts)", xlab = "")
   
   axis(1, at = c(1,1440,2880), labels = c("Thu","Fri","Sat"))
+  
+  dev.off()
   
   global_active
 }
